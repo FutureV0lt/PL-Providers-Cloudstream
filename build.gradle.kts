4df17d8fe17f3698744d39cb476b66cc57bdcc21
FutureV0lt <105385865+FutureV0lt@users.noreply.github.com>
2023-02-19 14:48:52 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ subprojects {
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         // you can modify it to use other git hosting services, like gitlab
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/user/repo")
+        setRepo(System.getenv("https://github.com/FutureV0lt/PL-Providers-Cloudstream") ?: "https://github.com/user/repo")
     }
 
     android {
